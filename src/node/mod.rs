48f4ba9ae7ef8fa6f1639a7d742bae064dab9328
i1i1 <vanyarybin1@live.ru>
2022-11-02 12:53:18 +0300

    Inline configuration creation

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -6,7 +6,6 @@ use std::sync::{Arc, Weak};
 
 use anyhow::Context;
 
-use sc_chain_spec::ChainSpec;
 use sc_executor::{WasmExecutionMethod, WasmtimeInstantiationStrategy};
 use sc_network::config::{NodeKeyConfig, Secret};
 use sc_service::config::{
@@ -80,6 +79,9 @@ impl Builder {
         directory: impl AsRef<Path>,
         chain_spec: ConsensusChainSpec<ConsensusGenesisConfig, ExecutionGenesisConfig>,
     ) -> anyhow::Result<Node> {
+        const NODE_KEY_ED25519_FILE: &str = "secret_ed25519";
+        const DEFAULT_NETWORK_CONFIG_PATH: &str = "network";
+
         let Self {
             mode: Mode::Full,
             name,
@@ -87,17 +89,111 @@ impl Builder {
             role: Role(role),
         } = self;
 
-        let mut full_client = subspace_service::new_full::<RuntimeApi, ExecutorDispatch>(
-            create_configuration(
-                BasePath::new(directory),
-                chain_spec,
-                name,
-                force_authoring,
-                role,
+        let base_path = BasePath::new(directory);
+        let impl_name = env!("CARGO_PKG_NAME").to_owned();
+        let impl_version = env!("CARGO_PKG_VERSION").to_string(); // TODO: include git revision here
+        let config_dir = base_path.config_dir(chain_spec.id());
+        let net_config_dir = config_dir.join(DEFAULT_NETWORK_CONFIG_PATH);
+        let client_id = format!("{}/v{}", impl_name, impl_version);
+        let mut network = NetworkConfiguration {
+            listen_addresses: vec![
+                "/ip6/::/tcp/30333".parse().expect("Multiaddr is correct"),
+                "/ip4/0.0.0.0/tcp/30333"
+                    .parse()
+                    .expect("Multiaddr is correct"),
+            ],
+            boot_nodes: chain_spec.boot_nodes().to_vec(),
+            ..NetworkConfiguration::new(
+                name.unwrap_or_default(),
+                client_id,
+                NodeKeyConfig::Ed25519(Secret::File(net_config_dir.join(NODE_KEY_ED25519_FILE))),
+                Some(net_config_dir),
             )
-            .await,
+        };
+
+        // Increase default value of 25 to improve success rate of sync
+        network.default_peers_set.out_peers = 50;
+        // Full + Light clients
+        network.default_peers_set.in_peers = 25 + 100;
+        let (keystore_remote, keystore) = (None, KeystoreConfig::InMemory);
+        let telemetry_endpoints = chain_spec.telemetry_endpoints().clone();
+
+        // Default value are used for many of parameters
+        let configuration = SubspaceConfiguration {
+            base: Configuration {
+                impl_name,
+                impl_version,
+                tokio_handle: tokio::runtime::Handle::current(),
+                transaction_pool: Default::default(),
+                network,
+                keystore_remote,
+                keystore,
+                database: DatabaseSource::ParityDb {
+                    path: config_dir.join("paritydb").join("full"),
+                },
+                state_cache_size: 67_108_864,
+                state_cache_child_ratio: None,
+                // TODO: Change to constrained eventually (need DSN for this)
+                state_pruning: Some(PruningMode::ArchiveAll),
+                blocks_pruning: BlocksPruning::All,
+                wasm_method: WasmExecutionMethod::Compiled {
+                    instantiation_strategy: WasmtimeInstantiationStrategy::PoolingCopyOnWrite,
+                },
+                wasm_runtime_overrides: None,
+                execution_strategies: ExecutionStrategies {
+                    syncing: ExecutionStrategy::AlwaysWasm,
+                    importing: ExecutionStrategy::AlwaysWasm,
+                    block_construction: ExecutionStrategy::AlwaysWasm,
+                    offchain_worker: ExecutionStrategy::AlwaysWasm,
+                    other: ExecutionStrategy::AlwaysWasm,
+                },
+                rpc_http: None,
+                rpc_ws: Some("127.0.0.1:9947".parse().expect("IP and port are valid")),
+                rpc_ipc: None,
+                // necessary in order to use `peers` method to show number of node peers during sync
+                rpc_methods: RpcMethods::Unsafe,
+                rpc_ws_max_connections: Default::default(),
+                // Below CORS are default from Substrate
+                rpc_cors: Some(vec![
+                    "http://localhost:*".to_string(),
+                    "http://127.0.0.1:*".to_string(),
+                    "https://localhost:*".to_string(),
+                    "https://127.0.0.1:*".to_string(),
+                    "https://polkadot.js.org".to_string(),
+                    "http://localhost:3009".to_string(),
+                ]),
+                rpc_max_payload: None,
+                rpc_max_request_size: None,
+                rpc_max_response_size: None,
+                rpc_id_provider: None,
+                ws_max_out_buffer_capacity: None,
+                prometheus_config: None,
+                telemetry_endpoints,
+                default_heap_pages: None,
+                offchain_worker: OffchainWorkerConfig::default(),
+                force_authoring,
+                disable_grandpa: false,
+                dev_key_seed: None,
+                tracing_targets: None,
+                tracing_receiver: TracingReceiver::Log,
+                chain_spec: Box::new(chain_spec),
+                max_runtime_instances: 8,
+                announce_block: true,
+                role,
+                base_path: Some(base_path),
+                informant_output_format: Default::default(),
+                runtime_cache_size: 2,
+                rpc_max_subs_per_conn: None,
+            },
+            force_new_slot_notifications: false,
+            dsn_config: None,
+        };
+
+        let slot_proportion = sc_consensus_slots::SlotProportion::new(2f32 / 3f32);
+        let mut full_client = subspace_service::new_full::<RuntimeApi, ExecutorDispatch>(
+            configuration,
             true,
-            sc_consensus_slots::SlotProportion::new(2f32 / 3f32),
+            slot_proportion,
         )
         .await
         .context("Failed to build a full subspace node")?;
@@ -128,116 +224,6 @@ impl Builder {
             rpc_handle,
             stop_sender,
         })
-    }
-}
-
-async fn create_configuration<CS: ChainSpec + 'static>(
-    base_path: BasePath,
-    chain_spec: CS,
-    node_name: Option<String>,
-    force_authoring: bool,
-    role: sc_service::Role,
-) -> SubspaceConfiguration {
-    const NODE_KEY_ED25519_FILE: &str = "secret_ed25519";
-    const DEFAULT_NETWORK_CONFIG_PATH: &str = "network";
-
-    let impl_name = env!("CARGO_PKG_NAME").to_owned();
-    let impl_version = env!("CARGO_PKG_VERSION").to_string(); // TODO: include git revision here
-    let config_dir = base_path.config_dir(chain_spec.id());
-    let net_config_dir = config_dir.join(DEFAULT_NETWORK_CONFIG_PATH);
-    let client_id = format!("{}/v{}", impl_name, impl_version);
-    let mut network = NetworkConfiguration {
-        listen_addresses: vec![
-            "/ip6/::/tcp/30333".parse().expect("Multiaddr is correct"),
-            "/ip4/0.0.0.0/tcp/30333"
-                .parse()
-                .expect("Multiaddr is correct"),
-        ],
-        boot_nodes: chain_spec.boot_nodes().to_vec(),
-        ..NetworkConfiguration::new(
-            node_name.unwrap_or_default(),
-            client_id,
-            NodeKeyConfig::Ed25519(Secret::File(net_config_dir.join(NODE_KEY_ED25519_FILE))),
-            Some(net_config_dir),
-        )
-    };
-
-    // Increase default value of 25 to improve success rate of sync
-    network.default_peers_set.out_peers = 50;
-    // Full + Light clients
-    network.default_peers_set.in_peers = 25 + 100;
-    let (keystore_remote, keystore) = (None, KeystoreConfig::InMemory);
-    let telemetry_endpoints = chain_spec.telemetry_endpoints().clone();
-
-    // Default value are used for many of parameters
-    SubspaceConfiguration {
-        base: Configuration {
-            impl_name,
-            impl_version,
-            tokio_handle: tokio::runtime::Handle::current(),
-            transaction_pool: Default::default(),
-            network,
-            keystore_remote,
-            keystore,
-            database: DatabaseSource::ParityDb {
-                path: config_dir.join("paritydb").join("full"),
-            },
-            state_cache_size: 67_108_864,
-            state_cache_child_ratio: None,
-            // TODO: Change to constrained eventually (need DSN for this)
-            state_pruning: Some(PruningMode::ArchiveAll),
-            blocks_pruning: BlocksPruning::All,
-            wasm_method: WasmExecutionMethod::Compiled {
-                instantiation_strategy: WasmtimeInstantiationStrategy::PoolingCopyOnWrite,
-            },
-            wasm_runtime_overrides: None,
-            execution_strategies: ExecutionStrategies {
-                syncing: ExecutionStrategy::AlwaysWasm,
-                importing: ExecutionStrategy::AlwaysWasm,
-                block_construction: ExecutionStrategy::AlwaysWasm,
-                offchain_worker: ExecutionStrategy::AlwaysWasm,
-                other: ExecutionStrategy::AlwaysWasm,
-            },
-            rpc_http: None,
-            rpc_ws: Some("127.0.0.1:9947".parse().expect("IP and port are valid")),
-            rpc_ipc: None,
-            // necessary in order to use `peers` method to show number of node peers during sync
-            rpc_methods: RpcMethods::Unsafe,
-            rpc_ws_max_connections: Default::default(),
-            // Below CORS are default from Substrate
-            rpc_cors: Some(vec![
-                "http://localhost:*".to_string(),
-                "http://127.0.0.1:*".to_string(),
-                "https://localhost:*".to_string(),
-                "https://127.0.0.1:*".to_string(),
-                "https://polkadot.js.org".to_string(),
-                "http://localhost:3009".to_string(),
-            ]),
-            rpc_max_payload: None,
-            rpc_max_request_size: None,
-            rpc_max_response_size: None,
-            rpc_id_provider: None,
-            ws_max_out_buffer_capacity: None,
-            prometheus_config: None,
-            telemetry_endpoints,
-            default_heap_pages: None,
-            offchain_worker: OffchainWorkerConfig::default(),
-            force_authoring,
-            disable_grandpa: false,
-            dev_key_seed: None,
-            tracing_targets: None,
-            tracing_receiver: TracingReceiver::Log,
-            chain_spec: Box::new(chain_spec),
-            max_runtime_instances: 8,
-            announce_block: true,
-            role,
-            base_path: Some(base_path),
-            informant_output_format: Default::default(),
-            runtime_cache_size: 2,
-            rpc_max_subs_per_conn: None,
-        },
-        force_new_slot_notifications: false,
-        dsn_config: None,
     }
 }
 
