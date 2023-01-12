5f97877c548b9499f2cc4c1762f6905133e1c602
i1i1 <vanyarybin1@live.ru>
2023-01-12 09:33:50 +0000

    Deploying to gh-pages from @ subspace/subspace-sdk@182914ed5beeda9e75c603ac7aeb697d3bf05edc 🚀

--- implementors/subspace_farmer/node_client/trait.NodeClient.js
+++ implementors/subspace_farmer/node_client/trait.NodeClient.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"subspace_sdk":[["impl RpcClient for <a class=\"struct\" href=\"subspace_sdk/node/struct.Node.html\" title=\"struct subspace_sdk::node::Node\">Node</a>"]]
+"subspace_sdk":[["impl NodeClient for <a class=\"struct\" href=\"subspace_sdk/node/struct.Node.html\" title=\"struct subspace_sdk::node::Node\">Node</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
