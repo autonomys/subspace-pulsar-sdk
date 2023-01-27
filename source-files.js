a45f790d13608f5f0947cc871fb9472e98446c6e
i1i1 <vanyarybin1@live.ru>
2023-01-27 20:12:37 +0000

    Deploying to gh-pages from @ subspace/subspace-sdk@a2ed02a7c550ed13e1657c059bd349d5ea887e35 🚀

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"subspace_sdk":["",[["node",[["domains",[],["core.rs","mod.rs"]]],["chain_spec.rs","mod.rs"]]],["farmer.rs","lib.rs","networking.rs","utils.rs"]]\
+"subspace_sdk":["",[["networking",[],["mod.rs","node_provider_storage.rs","provider_storage_utils.rs"]],["node",[["domains",[],["core.rs","mod.rs"]]],["chain_spec.rs","mod.rs"]]],["farmer.rs","lib.rs","utils.rs"]]\
 }');
 createSourceSidebar();
