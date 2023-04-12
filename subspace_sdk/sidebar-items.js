c24266c73b6c6e1464bcdb8e206cec138f85d532
i1i1 <vanyarybin1@live.ru>
2023-04-12 15:23:55 +0000

    Deploying to gh-pages from @ subspace/subspace-sdk@4b408084ba13c0bffba44bc9d7e54b09d7b20d45 🚀

--- subspace_sdk/sidebar-items.js
+++ subspace_sdk/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Ss58ParsingError"],"mod":["farmer","node"],"struct":["NodeBuilder","PublicKey"]};
+window.SIDEBAR_ITEMS = {"enum":["Ss58ParsingError"],"mod":["farmer","node"],"struct":["ByteSize","Multiaddr","MultiaddrWithPeerId","NodeBuilder","PublicKey"]};
