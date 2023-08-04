a8ff1089710f2cb07cf587bf51722891e47deb29
Parth Desai <desaiparth08@gmail.com>
2023-08-04 18:56:37 +0400

    import subspace/subspace#1689 subspace/subspace#1665 subspace/subspace#1686 subspace/subspace#1688

--- dsn/src/lib.rs
+++ dsn/src/lib.rs
@@ -15,11 +15,8 @@ mod provider_storage_utils;
 
 pub use builder::*;
 use either::*;
-use sc_client_api::AuxStore;
 /// Farmer piece cache
 pub use subspace_farmer::utils::farmer_piece_cache::FarmerPieceCache;
-use subspace_farmer_components::plotting::{PieceGetter, PieceGetterRetryPolicy};
-use subspace_networking::utils::piece_provider::PieceValidator;
 use subspace_networking::ParityDbProviderStorage;
 use tracing::warn;
 
@@ -41,38 +38,3 @@ pub type ProviderStorage<C> = provider_storage_utils::AndProviderStorage<
     provider_storage_utils::MaybeProviderStorage<FarmerProviderStorage>,
     NodeProviderStorage<C>,
 >;
-
-/// Node piece getter (combines DSN and Farmer getters)
-pub struct NodePieceGetter<PV, C> {
-    piece_getter: subspace_farmer::utils::node_piece_getter::NodePieceGetter<PV>,
-    node_cache: NodePieceCache<C>,
-}
-
-impl<PV, C> NodePieceGetter<PV, C> {
-    /// Constructor
-    pub fn new(
-        dsn_piece_getter: subspace_farmer::utils::node_piece_getter::NodePieceGetter<PV>,
-        node_cache: NodePieceCache<C>,
-    ) -> Self {
-        Self { piece_getter: dsn_piece_getter, node_cache }
-    }
-}
-
-#[async_trait::async_trait()]
-impl<PV: PieceValidator, C: AuxStore + Send + Sync> PieceGetter for NodePieceGetter<PV, C> {
-    async fn get_piece(
-        &self,
-        piece_index: subspace_core_primitives::PieceIndex,
-        retry_policy: PieceGetterRetryPolicy,
-    ) -> Result<
-        Option<subspace_core_primitives::Piece>,
-        Box<dyn std::error::Error + Send + Sync + 'static>,
-    > {
-        let piece = self.node_cache.get_piece(piece_index.hash()).map_err(|x| x.to_string())?;
-        if piece.is_some() {
-            return Ok(piece);
-        }
-
-        self.piece_getter.get_piece(piece_index, retry_policy).await
-    }
-}
