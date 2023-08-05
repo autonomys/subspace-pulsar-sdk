56d268209cea1d650a692cfe469986b908b357ca
Parth Desai <desaiparth08@gmail.com>
2023-08-05 07:54:42 +0400

    import subspace/subspace#1698 subspace/subspace#1699 subspace/subspace#1690 subspace/subspace#1697 subspace/subspace#1701 subspace/subspace#1707 subspace/subspace#1705

--- dsn/src/lib.rs
+++ dsn/src/lib.rs
@@ -14,27 +14,18 @@ mod builder;
 mod provider_storage_utils;
 
 pub use builder::*;
-use either::*;
 /// Farmer piece cache
 pub use subspace_farmer::utils::farmer_piece_cache::FarmerPieceCache;
-use subspace_networking::ParityDbProviderStorage;
 use tracing::warn;
 
 /// Node piece cache
 pub type NodePieceCache<C> = subspace_service::piece_cache::PieceCache<C>;
 /// Farmer provider storage
 pub type FarmerProviderStorage =
-    subspace_farmer::utils::farmer_provider_storage::FarmerProviderStorage<
-        ParityDbProviderStorage,
-        FarmerPieceCache,
-    >;
-/// Node provider storage
-pub type NodeProviderStorage<C> = subspace_service::dsn::node_provider_storage::NodeProviderStorage<
-    NodePieceCache<C>,
-    Either<ParityDbProviderStorage, subspace_networking::MemoryProviderStorage>,
->;
+    subspace_farmer::utils::farmer_provider_storage::FarmerProviderStorage<FarmerPieceCache>;
+
 /// General provider storage
 pub type ProviderStorage<C> = provider_storage_utils::AndProviderStorage<
     provider_storage_utils::MaybeProviderStorage<FarmerProviderStorage>,
-    NodeProviderStorage<C>,
+    NodePieceCache<C>,
 >;
