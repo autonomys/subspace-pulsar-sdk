a514c87d1b09751adcac6b1950d82bd1f64d2e48
ParthDesai <desaiparth08@gmail.com>
2023-11-02 11:11:31 +0000

    Deploying to gh-pages from @ subspace/subspace-sdk@3ef61e38dd45c215b2dbc059fc2bfafa4cd245f5 🚀

--- src-files.js
+++ src-files.js
@@ -1,7 +1,7 @@
 var srcIndex = JSON.parse('{\
 "sdk_dsn":["",[],["builder.rs","lib.rs","local_provider_record_utils.rs"]],\
 "sdk_farmer":["",[],["lib.rs"]],\
-"sdk_node":["",[["domains",[],["builder.rs","domain.rs","domain_genesis_block_builder.rs","domain_instance_starter.rs","domain_node.rs","evm_chain_spec.rs","evm_domain_executor_dispatch.rs","mod.rs","utils.rs"]]],["builder.rs","chain_spec.rs","lib.rs"]],\
+"sdk_node":["",[["domains",[],["builder.rs","domain.rs","domain_instance_starter.rs","domain_node.rs","evm_chain_spec.rs","evm_domain_executor_dispatch.rs","mod.rs","utils.rs"]]],["builder.rs","chain_spec.rs","lib.rs"]],\
 "sdk_substrate":["",[],["lib.rs","types.rs"]],\
 "sdk_traits":["",[],["lib.rs"]],\
 "sdk_utils":["",[],["lib.rs","rpc_client.rs"]],\
--- subspace_sdk/chain_spec/sidebar-items.js
+++ subspace_sdk/chain_spec/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["dev_config","devnet_config","devnet_config_compiled","gemini_3f","gemini_3f_compiled","local_config"],"struct":["GenesisParams"],"type":["ChainSpec"]};
+window.SIDEBAR_ITEMS = {"fn":["dev_config","devnet_config","devnet_config_compiled","gemini_3g","gemini_3g_compiled","local_config"],"struct":["GenesisParams"],"type":["ChainSpec"]};
