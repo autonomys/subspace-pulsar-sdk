7145764598e544bed4f3244d092c83b6a98fc042
ParthDesai <desaiparth08@gmail.com>
2023-12-15 21:50:48 +0000

    Deploying to gh-pages from @ subspace/subspace-sdk@2130db81d3733c1d18f45ebf1d19f88a36f132e0 🚀

--- src-files.js
+++ src-files.js
@@ -4,7 +4,7 @@ var srcIndex = JSON.parse('{\
 "sdk_node":["",[["domains",[],["builder.rs","domain.rs","domain_instance_starter.rs","domain_node.rs","evm_chain_spec.rs","evm_domain_executor_dispatch.rs","mod.rs","utils.rs"]]],["builder.rs","chain_spec.rs","lib.rs"]],\
 "sdk_substrate":["",[],["lib.rs","types.rs"]],\
 "sdk_traits":["",[],["lib.rs"]],\
-"sdk_utils":["",[],["lib.rs","rpc_client.rs"]],\
+"sdk_utils":["",[],["lib.rs"]],\
 "subspace_sdk":["",[],["lib.rs"]]\
 }');
 createSrcSidebar();
